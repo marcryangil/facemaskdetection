4468dbe7fbf16d7ad63a81208919f6fe07a5c4c8
Marc Ryan Gil <gil.marcryan@gmail.com>
2022-02-15 17:25:46 +0800

    new commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sqlite3
 import sys
 from PyQt5.uic import loadUi
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QStackedWidget
 import resources
-
 
 class LoginScreen(QMainWindow):
     def __init__(self):
         super(LoginScreen, self).__init__()
+        #self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        #self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         loadUi("login.ui", self)
-        # self.loginbtn.clicked.connect(self.gotologin)
         self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
         self.loginbtn.clicked.connect(self.loginfunction)
 
@@ -47,7 +47,53 @@ class DashboardScreen(QMainWindow):
     def __init__(self):
         super(DashboardScreen, self).__init__()
         loadUi("Dashboard.ui", self)
+        self.getstartedbtn.clicked.connect(self.open_file)
+        self.logsbtn.clicked.connect(self.gotoLogs)
 
+    def gotoLogs(self):
+        logs = LogScreen()
+        widget.addWidget(logs)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
+    def gotoLaunch(self):
+        import os
+        os.system('python detect/mask.py')
+
+
+    @QtCore.pyqtSlot()
+    def open_file(self):
+        url = QtCore.QUrl.fromLocalFile("Get Started.pdf")
+        QtGui.QDesktopServices.openUrl(url)
+
+
+class LogScreen(QMainWindow):
+    def __init__(self):
+        super(LogScreen, self).__init__()
+        loadUi("log.ui", self)
+        self.tableWidget.setHorizontalHeaderLabels(["ID", "Username", "Password"])
+        self.loaddata()
+        self.gobackbtn.clicked.connect(self.gotoDashboard)
+
+    def loaddata(self):
+        connection = sqlite3.connect("facemaskdetectionDB.db")
+        cur = connection.cursor()
+        sqlquery = "SELECT * FROM users"
+        tablerow = 0
+
+        self.tableWidget.setRowCount(3)
+
+        for row in cur.execute(sqlquery):
+            self.tableWidget.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(row[0]))
+            self.tableWidget.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(row[1]))
+            self.tableWidget.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(row[2]))
+            tablerow+=1
+
+        print(cur.execute(sqlquery).rowcount)
+
+    def gotoDashboard(self):
+        dashboard = DashboardScreen()
+        widget.addWidget(dashboard)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 # main
 app = QApplication(sys.argv)
