4db2e58d2e49e8894e1c01180a29642eb2e71d57
br4dd <earljohn321123@gmail.com>
2022-03-04 01:00:00 +0800

    error trapping on register

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ class LoginScreen(QMainWindow):
         widget.addWidget(dashboard)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-
 class DashboardScreen(QMainWindow):
     def __init__(self):
         super(DashboardScreen, self).__init__()
@@ -115,7 +114,8 @@ class RegisterScreen(QMainWindow):
         self.open_db()
         self.btn_back.clicked.connect(self.gotoDashboard)
         self.btn_save.clicked.connect(self.save_it)
-    
+        
+
     def open_db(self):
         # Create a database or connect to one
         conn = sqlite3.connect('facemaskdetectionDB.db')
@@ -123,7 +123,7 @@ class RegisterScreen(QMainWindow):
         c = conn.cursor()
 
         # Create table
-        c.execute("""CREATE TABLE if not exists user_list(
+        c.execute("""CREATE TABLE if not exists registered_user(
                 id_number TEXT,
                 first_name TEXT,
                 last_name TEXT,
@@ -137,43 +137,69 @@ class RegisterScreen(QMainWindow):
         # Close connection
         conn.close()
     
+    def has_error(self):
+        # added .replace(' ','') to remove whitespaces
+        id = self.line_id.text().replace(' ','')
+        first_name = self.line_first_name.text().replace(' ','')
+        last_name = self.line_last_name.text().replace(' ','')
+        return not bool(len(id) and len(first_name) and len(last_name))
+    
+    def is_select(self):
+        return self.comboBox_status_1.currentText() == 'Select'
     
     def save_it(self):
         
-        # Create a database or connect to one
-        conn = sqlite3.connect('facemaskdetectionDB.db')
-        # Create a cursor
-        c = conn.cursor()
-        
-        # Insert user to the database
-        c.execute("INSERT INTO user_list VALUES(:id_number, :first_name, :last_name, :status)",
-                  {
-                      'id_number': self.line_id.text(),
-                      'first_name': self.line_first_name.text(),
-                      'last_name': self.line_last_name.text(),
-                      'status': self.comboBox_status_1.currentText()
-                  }
-                  )
-                    
-        
-        print(self.line_id.text())
-        print(self.line_first_name.text())
-        print(self.line_last_name.text())
-        print(self.comboBox_status_1.currentText())
-        
-        
-        
-        # Commit changes
-        conn.commit()
-        # Close connection
-        conn.close()
-        
-        # Pop up message box
-        msg = QMessageBox()
-        msg.setWindowTitle('Saved to the Database!')
-        msg.setText('User has been saved')
-        msg.setIcon(QMessageBox.Information)
-        x = msg.exec_()
+        if self.has_error() or self.is_select():
+            self.label_error.setText('Error, please check fields')
+            if self.is_select():
+                self.comboBox_status_1.setStyleSheet(
+                                                    "color: rgb(0,0,0);\n"
+                                    "background-color: rgb(255,255,255);\n"
+                                        "border-style: solid;\n"
+                                        "border-width: 1px;\n"
+                                        "border-radius: 8px;\n"
+                                        "border-color: rgb(140, 140, 140)\n;"
+                                        "padding-left: 10px;\n"
+                                        "padding-right: 10px;\n"
+                                        "border-color: red;\n"
+                )
+        else:
+            self.comboBox_status_1.setStyleSheet(
+                                                 "color: rgb(0,0,0);\n"
+                                "background-color: rgb(255,255,255);\n"
+                                    "border-style: solid;\n"
+                                    "border-width: 1px;\n"
+                                    "border-radius: 8px;\n"
+                                    "border-color: rgb(140, 140, 140)\n;"
+                                    "padding-left: 10px;\n"
+                                    "padding-right: 10px;\n"
+                )
+            self.label_error.setText('')
+            # Create a database or connect to one
+            conn = sqlite3.connect('facemaskdetectionDB.db')
+            # Create a cursor
+            c = conn.cursor()
+            
+            # Insert user to the database
+            c.execute("INSERT INTO registered_user VALUES(:id_number, :first_name, :last_name, :status)",
+                    {
+                        'id_number': self.line_id.text(),
+                        'first_name': self.line_first_name.text(),
+                        'last_name': self.line_last_name.text(),
+                        'status': self.comboBox_status_1.currentText()
+                    }
+                    )            
+            # Commit changes
+            conn.commit()
+            # Close connection
+            conn.close()
+            
+            # Pop up message box
+            msg = QMessageBox()
+            msg.setWindowTitle('Saved to the Database!')
+            msg.setText('User has been saved')
+            msg.setIcon(QMessageBox.Information)
+            x = msg.exec_()
         
         
     def gotoDashboard(self):
